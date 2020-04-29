bf29a91c83834e77146ba691a32c8f294287cb20
Liam Stout <Liamstout212@gmail.com>
2020-04-29 12:59:03 +0100

    new favicon and added logos tomy worj

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -2,7 +2,12 @@ import React from 'react';
 import Styles from '../PageStyles.module.scss'
 import Navbar from '../NavBar'
 import projects from './projects'
-
+const logos = {
+  react: require('../../assets/img/react.svg'),
+  node: require('../../assets/img/node.png'),
+  python: require('../../assets/img/python.png'),
+  html: require('../../assets/img/html.png')
+}
 function Work() {
   return (
     <div>
@@ -12,7 +17,7 @@ function Work() {
           {projects.projects.map((project) => {
             return ( 
               <div className={Styles.WorkCard}>
-                <h3>{project.name}</h3>
+                <div style={{ display: 'flex', alignItems: 'center' }}><h3>{project.name}</h3>{project.tech.map((url) => { return <img style={{ marginLeft: '.3em', paddingBottom: '.3em' }} src={logos[url]} alt={'Tech'} height={20} /> })}</div>
                 <p>{project.description}</p>
                 <div className={Styles.WorkCardButtons}>
                   {project.live ? <a target="_blank" href={project.live}>Live Version</a> : <></>}
