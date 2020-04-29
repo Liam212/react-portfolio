8e093a96a43a25fe635a457cffa755a0d4b23ef0
Liam Stout <Liamstout212@gmail.com>
2020-04-29 11:50:44 +0100

    added new pages

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,7 +7,10 @@ function About() {
     <div>
         <Navbar />
         <div className={Styles.Container}>
-          <h1 style={{ color: '#FF9A9A' }}>about me</h1>
+          <h1 style={{ color: '#FF9A9A', marginBottom: '0' }}>about me</h1>
+          <div className={Styles.Body}>
+            <p>Hi, my name is Liam. I'm a Front-End Developer with a passion huge passion in creating effective user-friendly interfaces. I am learning new things every day while creating projects and opportunity. I will be updating my portfolio with new work as I move forward. I have developed my skills in HTML, CSS, and JavaScript to a high level. I also have knowledge of PHP and SQL. This can be seen in my example work below. Every day I am developing my existing skills further along with learning new things.</p>
+          </div>
         </div>
     </div>
   );
--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Styles from '../PageStyles.module.scss'
 import Navbar from '../NavBar'
+import projects from './projects'
 
 function Work() {
   return (
@@ -8,7 +9,19 @@ function Work() {
         <Navbar />
         <div className={Styles.Container}>
           <h1 style={{ color: '#FFC59A' }}>my work</h1>
-        </div>
+          {projects.projects.map((project) => {
+            return ( 
+              <div className={Styles.WorkCard}>
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+                <div className={Styles.WorkCardButtons}>
+                  {project.live ? <a target="_blank" href={project.live}>Live Version</a> : <></>}
+                  {project.source ? <a target="_blank" href={project.source}>Source Code</a> : <></>}
+                </div>
+              </div>
+            )
+          })}
+        </div>                
     </div>
   );
 }
