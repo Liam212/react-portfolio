ddbfc78a925cce890eb010da4d982938618c2b16
Liam Stout <Liamstout212@gmail.com>
2020-09-29 09:09:34 +0100

    updated projected

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import Styles from '../PageStyles.module.scss'
-import Navbar from '../NavBar'
-import projects from './projects'
+import React from "react";
+import Styles from "../PageStyles.module.scss";
+import Navbar from "../NavBar";
+import projects from "./projects";
 const logos = {
-  react: require('../../assets/img/react.svg'),
-  node: require('../../assets/img/node.png'),
-  python: require('../../assets/img/python.png'),
-  html: require('../../assets/img/html.png')
-}
+  react: require("../../assets/img/react.svg"),
+  node: require("../../assets/img/node.png"),
+  python: require("../../assets/img/python.png"),
+  html: require("../../assets/img/html.png"),
+  firebase: require("../../assets/img/firebase.png"),
+};
 function Work() {
   return (
     <div>
-        <Navbar />
-        <div className={Styles.Container}>
-          <h1 style={{ color: '#FFC59A' }}>my work</h1>
-          {projects.projects.map((project) => {
-            return ( 
-              <div className={Styles.WorkCard}>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}><h3>{project.name}</h3>{project.tech.map((url) => { return <img style={{ marginLeft: '.3em', paddingBottom: '.3em' }} src={logos[url]} alt={'Tech'} height={20} /> })}</div>
-                <p>{project.description}</p>
-                <div className={Styles.WorkCardButtons}>
-                  {project.live ? <a target="_blank" href={project.live}>Live Version</a> : <></>}
-                  {project.source ? <a target="_blank" href={project.source}>Source Code</a> : <a style={{ backgroundColor: '#FF9A9A' }} href='#'>Private Repo</a>}
-                </div>
+      <Navbar />
+      <div className={Styles.Container}>
+        <h1 style={{ color: "#FFC59A" }}>my work</h1>
+        {projects.projects.map((project) => {
+          return (
+            <div className={Styles.WorkCard}>
+              <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
+                <h3>{project.name}</h3>
+                {project.tech.map((url) => {
+                  return (
+                    <img
+                      style={{ marginLeft: ".3em", paddingBottom: ".3em" }}
+                      src={logos[url]}
+                      alt={"Tech"}
+                      height={20}
+                    />
+                  );
+                })}
               </div>
-            )
-          })}
-        </div>                
+              <p>{project.description}</p>
+              <div className={Styles.WorkCardButtons}>
+                {project.live ? (
+                  <a target="_blank" href={project.live}>
+                    Live Version
+                  </a>
+                ) : (
+                  <></>
+                )}
+                {project.source ? (
+                  <a target="_blank" href={project.source}>
+                    Source Code
+                  </a>
+                ) : (
+                  <a style={{ backgroundColor: "#FF9A9A" }} href="#">
+                    Private Repo
+                  </a>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
