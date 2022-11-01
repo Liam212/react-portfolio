3431b3f40c338536d680307c22a65fc522a1da93
liam212 <liamstout212@gmail.com>
2022-11-01 21:52:37 +0000

    Add Typescript and Fix Dom Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Work from './pages/Work/Work'
@@ -7,12 +7,12 @@ import Contact from './pages/Contact/Contact'
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/work" component={Work} />
-      <Route exact path="/contact" component={Contact} />
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/work" element={<Work />} />
+      <Route exact path="/contact" element={<Contact />} />
+    </Routes>
   );
 }
 
